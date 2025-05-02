32ecc2e9b6ae956639dbc0eff169a1a8e7c4b652
Mohamed Elsaka <mohammed.yousry10@gmail.com>
2025-05-03 02:21:54 +0300

    add  tailwind configurations and styles

---
+++ apps/assets/tailwind.config.js
@@ -0,0 +1,17 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+const sharedTailwindConfig = require('../../libs/shared/tailwind-presets/tailwind.config');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    presets: [sharedTailwindConfig],
+    content: [
+        join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+        ...createGlobPatternsForDependencies(__dirname),
+        'libs/assets/dashboard/src/**/!(*.stories|*.spec).{ts,html}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
---
+++ libs/shared/tailwind-presets/tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                surface: 'hsl(0, 0%, 100%)',
+                hover: 'hsl(0, 0%, 96%)',
+                'on-surface': 'hsl(195, 3%, 24%)',
+                border: 'hsl(210, 0%, 89%)',
+                primary: {
+                    DEFAULT: 'hsl(213, 97%, 53%)',
+                    600: 'hsl(213, 97%, 53%)',
+                },
+            },
+
+            fontSize: {
+                base: '14px',
+            },
+
+            borderRadius: {
+                DEFAULT: '3px',
+            },
+            spacing: {
+                'item-selector': '275px',
+            },
+        },
+    },
+    plugins: [],
+};
